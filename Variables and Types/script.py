ac971c42e198b3af8d56350745e5bc3dca7f2980
JanRodo46 <michal.96119@wp.pl>
2025-10-15 21:21:48 +0200

    2 scripts added

--- Variables and Types/script.py
+++ Variables and Types/script.py
@@ -2,3 +2,7 @@ myfloat = 7.0
 print(myfloat)
 myfloat = float(7)
 print(myfloat)
+
+biegar = "ok"
+print(biegar)
+print(biegar + " ""+""1")
---
+++ Variables and Types/scripy2.py
@@ -0,0 +1,10 @@
+name = "Jacob"
+surname = "Byegurchyck"
+coolname = name + " " + surname
+print(coolname)
+# This will not work!
+one = 1
+two = 2
+hello = "hello"
+
+print(one + two)
