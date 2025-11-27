789876fc6833fd9da0c6e9a41cc3aca34a629b72
JanRodo46 <michal.96119@wp.pl>
2025-11-27 18:22:40 +0100

    Changed using f strings

--- String_Formatting/script.py
+++ String_Formatting/script.py
@@ -1,17 +1,15 @@
 # This prints out "Hello, John!"
 name = "John"
-print("Hello, %s!" % name)
+print(f"Hello, {name}")
 
 # This prints out "John is 23 years old."
 name = "John"
 age = 23
-print("%s is %d years old." % (name, age))
+print(f"{name} is {age} years old.")
 
 # This prints out: A list: [1, 2, 3]
 mylist = [1, 2, 3]
-print("A list: %s" % mylist)
+print(f"A list: {mylist}")
 
 data = ("John", "Doe", 53.44)
-format_string = "Hello %s %s. Your current balance is $%.2f."
-
-print(format_string % data)
+print(f"Hello {data[0]} {data[1]}. Your current ballance is ${data[2]:.2f}")
