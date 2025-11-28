bf9ec0a49d93b91060067ea9479bcc8189b1a578
JanRodo46 <michal.96119@wp.pl>
2025-11-28 16:36:31 +0100

    Correct solution

--- Lists/script.py
+++ Lists/script.py
@@ -3,10 +3,15 @@ strings = []
 names = ["John", "Eric", "Jessica"]
 
 # write your code here
-second_name = None
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("hello")
+strings.append("world")
+second_name = names[1]
 
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
 print(numbers)
 print(strings)
-print("The second name on the names list is %s" % second_name)
+print(f"The second name on the names list is {second_name}")
